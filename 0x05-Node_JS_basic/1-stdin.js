082c8e7b5cc477b2900c598388f6597987b1dd3c
NikkyXO <alexisdame2017@gmail.com>
2023-02-22 01:00:44 +0100

    Node js basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// listens for input from the user using the process.stdin.on() method
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-  } else {
-    process.stdout.write('Please enter your name: \n');
-  }
-});
-
-// program listens for the exit event using the process.on()
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
