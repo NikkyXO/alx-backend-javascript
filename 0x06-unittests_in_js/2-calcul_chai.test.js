0570404ae6f39f9f0ada280fa86c7d470cf77662
NikkyXO <alexisdame2017@gmail.com>
2023-02-25 09:10:25 +0100

    Unittesting in js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,9 +1,8 @@
-const assert = require("assert");
 const expect = require("chai").expect;
 const { describe, it } = require("mocha");
-const calculateNumber = require("./1-calcul");
+const calculateNumber = require("./2-calcul_chai");
 
-describe("calculateNumber()", function () {
+describe("calculateNumber", function () {
 	describe("SUM", function () {
 		it("checking if numbers round", function () {
 			expect(calculateNumber("SUM", 1, 2)).to.equal(3);
@@ -11,33 +10,27 @@ describe("calculateNumber()", function () {
 		it("checking if numbers round", function () {
 			expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
 		});
-	})
+	});
 	describe("SUBTRACT", function () {
 		it("checking if numbers round", function () {
-			it("checking if numbers round", function () {
-				expect(calculateNumber("SUBTRACT", 1.4, 2.2).to.equal(-1));
-			});
+			expect(calculateNumber("SUBTRACT", 1.4, 2.3)).to.equal(-1);
 		});
 		it("checking if numbers round", function () {
-			it("checking if numbers round", function () {
-				expect(calculateNumber("SUBTRACT", 4.9, 2.7).to.equal(2));
-			});
+			expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
+		});
+		it("checking if numbers round", function () {
+			expect(calculateNumber("SUBTRACT", -4.9, -2.7)).to.equal(-2);
 		});
 	});
-
 	describe("DIVIDE", function () {
 		it("checking if numbers round", function () {
-			const result = calculateNumber("DIVIDE", 4, 2);
-			assert.strictEqual(result, 2);
+			expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
 		});
 		it("checking if numbers round", function () {
-			const result = calculateNumber("DIVIDE", 4, 0);
-			assert.strictEqual(result, "Error");
+			expect(calculateNumber("DIVIDE", 4.6, 1.8)).to.equal(2.5);
 		});
 		it("checking if numbers round", function () {
-			const result = calculateNumber("DIVIDE", 4.6, 1.8);
-			assert.strictEqual(result, 2.5);
+			expect(calculateNumber("DIVIDE", 4, 0)).to.equal("Error");
 		});
 	});
-	
 });
